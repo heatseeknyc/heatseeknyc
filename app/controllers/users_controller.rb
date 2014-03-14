0bd5cb27fdaa49534c855be48228adde076f22f5
Tristan Siegel <tristan.t.siegel@gmail.com>
2014-03-14 12:38:26 -0400

    adds correct mobile form for new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+
+  def index
+    
+  end
+
+  def new
+    @user = User.new
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   has_many :readings
+
+  def twine
+    
+  end
 end
