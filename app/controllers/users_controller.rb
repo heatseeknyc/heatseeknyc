cb09bd577a7b7eb96ab2bd1979c4ebe1fa0d7ffa
Tristan Siegel <tristan.t.siegel@gmail.com>
2014-03-15 14:40:09 -0400

    finished views for form page and adds validations for the form page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,32 @@
 class UsersController < ApplicationController
+  # before_action :set_user, only: [:show, :new, :update, :destroy]
 
   def index
-    
+    @users = User.all
   end
 
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(new_user_params)
+    if @user.valid?
+      @user.save
+      redirect_to "/users"
+    else
+      @user
+      render action: 'new'
+    end
+  end
+
+  private
+    def set_user
+      
+    end
+
+    def new_user_params
+      params.require(:user).permit(:first_name, :last_name, :address, :email)
+    end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
   has_many :readings
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates_presence_of :address
+  validates_presence_of :email
+
 
   def twine
     
