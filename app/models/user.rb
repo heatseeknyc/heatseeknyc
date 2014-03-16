c0b249e67535ab2bdf1c063d496ddd5dfeac02f1
wfjeff <wm.jeffries@gmail.com>
2014-03-16 15:44:33 -0400

    use inject instead of to_h in readings_hash function to make ruby 2.0.0 compatible

--- app/models/user.rb
+++ app/models/user.rb
@@ -95,13 +95,20 @@ class User < ActiveRecord::Base
     self.readings.map(&:created_at)
   end
 
-  def reading_hash
-    self.reading_nested_array.to_h
-  end
+  # def reading_hash
+  #   self.reading_nested_array.to_h
+  # end
 
-  def reading_nested_array
-    self.readings.map do |reading|
-      [reading.created_at, reading.temp]
+  # def reading_nested_array
+  #   self.readings.map do |reading|
+  #     [reading.created_at, reading.temp]
+  #   end
+  # end
+
+  def reading_hash
+    self.readings.inject({}) do |reading_hash, reading|  
+      reading_hash[reading.created_at] = reading.temp
+      reading_hash
     end
   end
 
