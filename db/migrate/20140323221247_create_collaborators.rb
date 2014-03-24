d5364e0dc4ed93b278074ba2e3a8a2f834c8ac1f
Tristan Siegel <tristan.t.siegel@gmail.com>
2014-03-24 12:29:48 -0400

    removes duplicate migration

--- db/migrate/20140323221247_create_collaborators.rb
+++
@@ -1,9 +0,0 @@
-class CreateCollaborators < ActiveRecord::Migration
-  def change
-    create_table :user_collaborators do |t|
-      t.integer :user_id
-      t.integer :collaborator_id
-      t.integer :permissions
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -13,17 +13,11 @@
 
 ActiveRecord::Schema.define(version: 20140322233815) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "collaborations", force: true do |t|
     t.integer  "user_id"
     t.integer  "collaborator_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "collaborators_tables", force: true do |t|
   end
 
   create_table "readings", force: true do |t|
@@ -58,6 +52,6 @@ ActiveRecord::Schema.define(version: 20140322233815) do
     t.string   "email",                  default: "", null: false
   end
 
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
