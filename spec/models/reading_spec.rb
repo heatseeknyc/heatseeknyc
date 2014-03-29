21009b4a329816725e8937130d09a285df9c0503
wfjeff <wm.jeffries@gmail.com>
2014-03-28 20:46:20 -0400

    add tests for readings validations

--- spec/models/reading_spec.rb
+++ spec/models/reading_spec.rb
@@ -11,4 +11,25 @@ describe Reading do
     reading = create(:reading)
     expect(reading.created_at).to be_an_instance_of(ActiveSupport::TimeWithZone)
   end
+
+  it "cannot be created without a temperature" do
+    tom = create(:user)
+    twine1 = create(:twine)
+    reading = Reading.create(user: tom, twine: twine1)
+    expect(reading.persisted?).to eq false
+  end
+
+  it "cannot be created without a user" do
+    temp = 53
+    twine1 = create(:twine)
+    reading = Reading.create(temp: temp, twine: twine1)
+    expect(reading.persisted?).to eq false
+  end
+
+  it "cannot be created without a twine" do
+    temp = 53
+    tom = create(:user)
+    reading = Reading.create(temp: temp, user: tom)
+    expect(reading.persisted?).to eq false
+  end
 end
