4d58b9969cda5ccf9b83b251d1dffbaef1e410d2
wfjeff <wm.jeffries@gmail.com>
2014-04-03 12:17:45 -0400

    add validation so collaborations cannot be between user and his or herself

--- app/models/collaboration.rb
+++ app/models/collaboration.rb
@@ -1,4 +1,9 @@
 class Collaboration < ActiveRecord::Base
   belongs_to :user
   belongs_to :collaborator, :class_name => "User"
+  before_save :valid_relationship?
+
+  def valid_relationship?
+    self.user_id != self.collaborator_id
+  end
 end
