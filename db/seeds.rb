be229e904785de939d5835b715778bb1d7923178
wfjeff <wm.jeffries@gmail.com>
2014-04-03 21:03:24 -0400

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+jane = User.find_by(email: "jane@heatseeknyc.com")
+john = User.find_by(email: "john@heatseeknyc.com")
+now = Time.now
+current_time = now - (now.to_i % 3600)
+users = [jane, john]
+users.each do |user|
+  user.readings.clear
+  current_temp = 65
+  current_outdoor_temp = 40
+  200.times do
+    reading = Reading.create(created_at: current_time, temp: current_temp, outdoor_temp: current_outdoor_temp, user: user, twine: user.twine)
+    current_time -= 3600
+    if user.during_the_day?(current_time)
+      current_temp += rand(-3..2)
+      current_outdoor_temp += rand(-3..2)
+    else
+      current_temp += rand(-2..3)
+      current_outdoor_temp += rand(-2..3)
+    end
+
+    current_temp -= 2 if current_temp > 68
+    current_temp += 2 if current_temp < 52
+  end
+end
