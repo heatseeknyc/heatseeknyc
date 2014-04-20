df57b4807920954009e567c76181259519240c1f
wfjeff <wm.jeffries@gmail.com>
2014-04-19 21:18:19 -0400

    add unit test for collaboration model

--- spec/models/collaboration_spec.rb
+++ spec/models/collaboration_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe Collaboration do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#valid_relationship?" do
+    context 'when user and collaborator are the same' do
+      it "returns false" do
+        collab = Collaboration.create(user_id: 1, collaborator_id: 1)
+        expect(collab.valid_relationship?).to eq false
+      end
+    end
+
+    context 'when user and collaborator are different' do
+      it "returns false" do
+        collab = Collaboration.create(user_id: 1, collaborator_id: 2)
+        expect(collab.valid_relationship?).to eq true
+      end
+    end
+  end
 end
