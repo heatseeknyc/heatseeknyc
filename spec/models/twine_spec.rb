20e8465362eb37f472ad87a67f5b45fef188f736
wfjeff <wm.jeffries@gmail.com>
2014-04-19 21:19:12 -0400

    add back in twine unit tests

--- spec/models/twine_spec.rb
+++ spec/models/twine_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Twine do
   
-  xit "has readings" do
+  it "has readings" do
     twine1 = create(:twine)
     reading1 = create(:reading, twine: twine1)
     reading2 = create(:reading, twine: twine1)
@@ -10,13 +10,13 @@ describe Twine do
   end
 
   describe "#get_reading" do
-    xit "makes a new reading" do
+    it "makes a new reading" do
       twine_with_reading = create(:twine, email: "wm.jeffries+1@gmail.com")
       reading1 = twine_with_reading.get_reading
       expect(twine_with_reading.readings.first).to eq reading1
     end
 
-    xit "assigns readings to correct user" do
+    it "assigns readings to correct user" do
       twine1 = create(:twine)
       reading = twine1.get_reading
       expect(reading.user).to eq twine1.user
