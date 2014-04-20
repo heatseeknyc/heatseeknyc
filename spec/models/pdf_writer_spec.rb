6fa0478d930a1cfaf4656f0d3401a7d119063a14
wfjeff <wm.jeffries@gmail.com>
2014-04-19 23:35:10 -0400

    make pdf writer spec pass

--- spec/models/pdf_writer_spec.rb
+++ spec/models/pdf_writer_spec.rb
@@ -4,6 +4,7 @@ describe PDFWriter do
   describe "#generate_pdf" do
     it "generates a pdf" do
       user = create(:user)
+      user.readings << create(:reading)
       writer = PDFWriter.new_from_user_id(user.id)
       expect(writer.generate_pdf).to be_an_instance_of(String)
     end
