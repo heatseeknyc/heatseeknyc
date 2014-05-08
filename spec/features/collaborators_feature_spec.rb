e090de52c64d4b25fa01280c9e780a5748a4feae
Tristan Siegel <tristan.t.siegel@gmail.com>
2014-05-08 14:24:30 -0400

    changes back the code from previous commit

--- spec/features/collaborators_feature_spec.rb
+++ spec/features/collaborators_feature_spec.rb
@@ -16,13 +16,12 @@ describe "viewing your own page" do
   it "shows you a table with accurate summary data" do
     james = create(:user, first_name: "James")
     login_as(james, scope: :user)
-    Reading.delete_all
-    create(:reading, :day_time, user: james, temp: 55)
+    create(:reading, :day_time, user: james, temp: 56)
     create(:reading, :day_time, user: james, temp: 57)
     create(:reading, :night_time, user: james, temp: 51)
     create(:reading, :night_time, user: james, temp: 53)
     visit user_path(james)
-    expect(page).to have_content "55"
+    expect(page).to have_content "56"
     expect(page).to have_content "57"
     expect(page).to have_content "51"
     expect(page).to have_content "53"
