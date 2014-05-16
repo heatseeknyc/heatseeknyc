5ac12253afdbe961dcedb86ae8c41cec0b422b10
Tristan Siegel <tristan.t.siegel@gmail.com>
2014-05-15 21:31:00 -0400

    adds js functions to map coordinates, needs to fix syntax error on first function but stopping for the night

--- app/assets/javascripts/complaint.js
+++ app/assets/javascripts/complaint.js
@@ -1,7 +1,23 @@
+$(document).ready(function(){
+  // complaints is a variable made available to the complaints/index.html.erb
+  addComplaintsToMap(complaints);
+}
+
 // converts complaint lat/long to latlng objects in an array
-array = [];
-complaints.forEach(function(arr){
- array.push(L.latLng(arr[0], arr[1]));
-});
+function createLatLngs(complaintsCoordinates){
+  var convertedLatLngArray = [];
+  complaints.forEach(function(complaintsCoordinates){
+    convertedLatLngArray.push(
+      L.latLng(complaintsCoordinates[0], complaintsCoordinates[1])
+    );
+  });
+  return convertedLatLngArray;
+};
+
 // adds complaints to map
-L.heatLayer(array, {radius: 15, gradient: {0.4: 'blue', 0.65: 'lime', 1: 'red'}}).addTo(map);
+function addComplaintsToMap(complaintsCoordinates){
+  var convertedComplaints = createLatLngs(complaintsCoordinates);
+  L.heatLayer(
+    convertedComplaints, {radius: 10, gradient: {0.4: 'blue', 0.65: 'lime', 1: 'red'}}
+  ).addTo(map);
+};
