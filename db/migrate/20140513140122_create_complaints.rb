686b58300652726763d2cbedfccff26b6bc009bc
Tristan Siegel <tristan.t.siegel@gmail.com>
2014-05-21 11:51:08 -0400

    uncomments and alters complaint migration

--- db/migrate/20140513140122_create_complaints.rb
+++ db/migrate/20140513140122_create_complaints.rb
@@ -1,20 +1,20 @@
 class CreateComplaints < ActiveRecord::Migration
-  # def change
-  #   create_table :complaints do |t|
-  #     t.date    "created_date"
-  #     t.date    "closed_date"
-  #     t.string  "agency"
-  #     t.string  "agency_name"
-  #     t.string  "complaint_type"
-  #     t.string  "descriptor"
-  #     t.string  "location_type"
-  #     t.integer "incident_zip"
-  #     t.string  "incident_address"
-  #     t.string  "street_name"
-  #     t.string  "community_board"
-  #     t.string  "borough"
-  #     t.float   "latitude"
-  #     t.float   "longitude"
-  #   end
-  # end
+  def change
+    create_table :complaints do |t|
+      t.date    "created_date"
+      t.date    "closed_date"
+      t.string  "agency"
+      t.string  "agency_name"
+      t.string  "complaint_type"
+      t.string  "descriptor"
+      t.string  "location_type"
+      t.integer "incident_zip"
+      t.string  "incident_address"
+      t.string  "street_name"
+      t.string  "community_board"
+      t.string  "borough"
+      t.decimal "latitude", precision: 15
+      t.decimal "longitude", precision: 15
+    end
+  end
 end
