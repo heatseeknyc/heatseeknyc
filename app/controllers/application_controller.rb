82901c745f8892873f0a883a49641bffde8da6c6
Daniel Kronovet <kronovet@gmail.com>
2014-06-05 17:41:47 -0400

    added route and view for zoho email verification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+  def zoho
+    render "misc/zoho"
+  end
+
   protected
 
   def configure_permitted_parameters
