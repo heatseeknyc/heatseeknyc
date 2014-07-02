98d7f5b89209211a7e709b846367dc7bc4be8388
wfjeff <wm.jeffries@gmail.com>
2014-07-02 15:43:03 -0400

    add video folder to asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(:default, Rails.env)
 
 module Twinenyc
   class Application < Rails::Application
+    config.assets.paths << "#{Rails.root}/app/assets/videos"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
