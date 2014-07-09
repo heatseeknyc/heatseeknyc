fe9906af58c640e2e4c56d59dc6b1770948d8626
wfjeff <wm.jeffries@gmail.com>
2014-07-09 17:37:44 -0400

    update about page for new design

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,6 @@ Twinenyc::Application.routes.draw do
   get "sponsors" => "welcome#sponsors"
   get "team" => "welcome#team"
   get "about" => "welcome#about"
-  get "how-it-works" => "welcome#how_it_works"
   get "vote-for-us" => "welcome#vote_for_us"
   get "demo" => "welcome#demo"
   root 'welcome#index'
