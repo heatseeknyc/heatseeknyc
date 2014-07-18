220ec52b074b7282829852284d545f595d797569
Ethan Ozelius <e.ozelius@gmail.com>
2014-07-18 18:14:26 -0400

    Advocate temp
    description

--- db/schema.rb
+++ db/schema.rb
@@ -60,12 +60,6 @@ ActiveRecord::Schema.define(version: 20140702194735) do
     t.string  "email"
   end
 
-  create_table "user_collaborators", force: true do |t|
-    t.integer "user_id"
-    t.integer "collaborator_id"
-    t.integer "permissions"
-  end
-
   create_table "users", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
