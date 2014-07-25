c0a8db619aa105df03a2c7f1044b0fe6fd78dc05
wfjeff <wm.jeffries@gmail.com>
2014-07-25 17:45:14 -0400

    redirect to user page after successful login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
-  def zoho
-    render "misc/zoho"
+  def after_sign_in_path_for(resource)
+    user_path(current_user)
   end
 
   protected
