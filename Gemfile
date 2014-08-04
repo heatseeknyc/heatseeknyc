cb0e496fd2bef61c8bcb7b3d79244d7171188ef8
wfjeff <wm.jeffries@gmail.com>
2014-08-03 22:43:38 -0400

    wip implement paginated blog with dates

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+gem 'will_paginate'
+gem 'tumblr_client', '0.8.1'
 gem 'google-analytics-rails'
 gem 'coveralls', require: false
 gem 'wunderground'
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,24 @@
 class WelcomeController < ApplicationController
+  require "will_paginate/array"
+
   def index
     # redirect_to current_user if current_user
     # render "current_user_index" if current_user
+  end
+
+  def blog
+    client = Tumblr::Client.new({
+      :consumer_key => 'CEOhpSLV95qmGJqfiDU2OFVohJctU0h2yN5gku605aeeoXfiMB',
+      :consumer_secret => '404m3BAXx4HRv0YuzBtbk2YdgG4rPrhys7SCFkHugOlM9WkYsO',
+      :oauth_token => 'Aea7rOSFfEvAfE5MmBiYIHHQl4BkKHzdcyfZrZtgx7p2jWyBoM',
+      :oauth_token_secret => 'mJOy2poC33vsLRfRtpWryqeyYUAK9S2fVA23fsIVkjGbqpGnJe'
+    })
+    result = client.posts('heatseeknyc.tumblr.com')['posts']
+    
+    @entries = WillPaginate::Collection.create(1, 4, result.count) do |pager|
+      pager.replace(result)
+    end
+
+    @entries.paginate(:page => params[:page], :per_page => 4)
   end
 end
