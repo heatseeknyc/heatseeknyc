d0e359fcf77bc1b34983b1caf3c76d59c560f809
Tristan Siegel <tristan.t.siegel@gmail.com>
2014-08-04 22:42:41 -0400

    begins adding a different d3js chart

--- app/assets/javascripts/d3-chart.js
+++ app/assets/javascripts/d3-chart.js
@@ -21,74 +21,217 @@ D3Chart.prototype.setLine = function(first_argument) {
 };
 */
 
+var w = 900,
+    h = 450;
 
-// example d3 chart
-  var m = [80, 80, 80, 80]; // margins
-  var w = 1000 - m[1] - m[3]; // width
-  var h = 400 - m[0] - m[2]; // height
-  
-  // create a simple data array that we'll plot with a line (this array represents only the Y values, X will just be the index location)
+var monthNames = [ "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December" ];
+
+var maxDataPointsForDots = 50,
+  transitionDuration = 1000;
+
+var svg = null,
+  yAxisGroup = null,
+  xAxisGroup = null,
+  dataCirclesGroup = null,
+  dataLinesGroup = null;
+
+function draw(response) {
+  var data = response;
+  response.forEach(function(obj){
+    obj.date = new Date(obj.created_at);
+  });
+
+  var margin = 40;
+  var max = d3.max(data, function(d) { return d.temp }) + 10;
+  var min = d3.min(data, function(d) { return d.temp }) - 10;
+  var pointRadius = 4;
+  var x = d3.time.scale().range([0, w - margin * 2]).domain([data[0].date, data[data.length - 1].date]);
+  var y = d3.scale.linear().range([h - margin * 2, 0]).domain([min, max]);
+
+  var xAxis = d3.svg.axis().scale(x).tickSize(h - margin * 2).tickPadding(10).ticks(7);
+  var yAxis = d3.svg.axis().scale(y).orient('left').tickSize(-w + margin * 2).tickPadding(10);
+  var t = null;
+
+  svg = d3.select('#chart').select('svg').select('g');
+  if (svg.empty()) {
+    svg = d3.select('#chart')
+      .append('svg:svg')
+        .attr('width', w)
+        .attr('height', h)
+        .attr('class', 'viz')
+      .append('svg:g')
+        .attr('transform', 'translate(' + margin + ',' + margin + ')');
+  }
+
+  t = svg.transition().duration(transitionDuration);
+
+  // y ticks and labels
+  if (!yAxisGroup) {
+    yAxisGroup = svg.append('svg:g')
+      .attr('class', 'yTick')
+      .call(yAxis);
+  }
+  else {
+    t.select('.yTick').call(yAxis);
+  }
+
+  // x ticks and labels
+  if (!xAxisGroup) {
+    xAxisGroup = svg.append('svg:g')
+      .attr('class', 'xTick')
+      .call(xAxis);
+  }
+  else {
+    t.select('.xTick').call(xAxis);
+  }
+
+  // Draw the lines
+  if (!dataLinesGroup) {
+    dataLinesGroup = svg.append('svg:g');
+  }
+
+  var dataLines = dataLinesGroup.selectAll('.data-line')
+      .data([data]);
+
+  var line = d3.svg.line()
+    // assign the X function to plot our line as we wish
+    .x(function(d,i) { 
+      // verbose logging to show what's actually being done
+      //console.log('Plotting X temp for date: ' + d.date + ' using index: ' + i + ' to be at: ' + x(d.date) + ' using our xScale.');
+      // return the X coordinate where we want to plot this datapoint
+      //return x(i); 
+      return x(d.date); 
+    })
+    .y(function(d) { 
+      // verbose logging to show what's actually being done
+      //console.log('Plotting Y temp for data temp: ' + d.temp + ' to be at: ' + y(d.temp) + " using our yScale.");
+      // return the Y coordinate where we want to plot this datapoint
+      //return y(d); 
+      return y(d.temp); 
+    })
+    .interpolate("linear");
+
+     /*
+     .attr("d", d3.svg.line()
+     .x(function(d) { return x(d.date); })
+     .y(function(d) { return y(0); }))
+     .transition()
+     .delay(transitionDuration / 2)
+     .duration(transitionDuration)
+      .style('opacity', 1)
+                        .attr("transform", function(d) { return "translate(" + x(d.date) + "," + y(d.temp) + ")"; });
+      */
+
+  var garea = d3.svg.area()
+    .interpolate("linear")
+    .x(function(d) { 
+      // verbose logging to show what's actually being done
+      return x(d.date); 
+    })
+              .y0(h - margin * 2)
+    .y1(function(d) { 
+      // verbose logging to show what's actually being done
+      return y(d.temp); 
+    });
+
+  dataLines
+    .enter()
+    .append('svg:path')
+              .attr("class", "area")
+              .attr("d", garea(data));
+
+  dataLines.enter().append('path')
+     .attr('class', 'data-line')
+     .style('opacity', 0.3)
+     .attr("d", line(data));
+    /*
+    .transition()
+    .delay(transitionDuration / 2)
+    .duration(transitionDuration)
+      .style('opacity', 1)
+      .attr('x1', function(d, i) { return (i > 0) ? xScale(data[i - 1].date) : xScale(d.date); })
+      .attr('y1', function(d, i) { return (i > 0) ? yScale(data[i - 1].temp) : yScale(d.temp); })
+      .attr('x2', function(d) { return xScale(d.date); })
+      .attr('y2', function(d) { return yScale(d.temp); });
+    */
+
+  dataLines.transition()
+    .attr("d", line)
+    .duration(transitionDuration)
+      .style('opacity', 1)
+                        .attr("transform", function(d) { return "translate(" + x(d.date) + "," + y(d.temp) + ")"; });
+
+  dataLines.exit()
+    .transition()
+    .attr("d", line)
+    .duration(transitionDuration)
+                        .attr("transform", function(d) { return "translate(" + x(d.date) + "," + y(0) + ")"; })
+      .style('opacity', 1e-6)
+      .remove();
+
+  d3.selectAll(".area").transition()
+    .duration(transitionDuration)
+    .attr("d", garea(data));
+
+  // Draw the points
+  if (!dataCirclesGroup) {
+    dataCirclesGroup = svg.append('svg:g');
+  }
+
+  var circles = dataCirclesGroup.selectAll('.data-point')
+    .data(data);
+
+  circles
+    .enter()
+      .append('svg:circle')
+        .attr('class', 'data-point')
+        .style('opacity', 1e-6)
+        .attr('cx', function(d) { return x(d.date) })
+        .attr('cy', function() { return y(0) })
+        .attr('r', function() { return (data.length <= maxDataPointsForDots) ? pointRadius : 0 })
+      .transition()
+      .duration(transitionDuration)
+        .style('opacity', 1)
+        .attr('cx', function(d) { return x(d.date) })
+        .attr('cy', function(d) { return y(d.temp) });
+
+  circles
+    .transition()
+    .duration(transitionDuration)
+      .attr('cx', function(d) { return x(d.date) })
+      .attr('cy', function(d) { return y(d.temp) })
+      .attr('r', function() { return (data.length <= maxDataPointsForDots) ? pointRadius : 0 })
+      .style('opacity', 1);
+
+  circles
+    .exit()
+      .transition()
+      .duration(transitionDuration)
+        // Leave the cx transition off. Allowing the points to fall where they lie is best.
+        //.attr('cx', function(d, i) { return xScale(i) })
+        .attr('cy', function() { return y(0) })
+        .style("opacity", 1e-6)
+        .remove();
+
+    //   $('svg circle').tipsy({ 
+    //     gravity: 'w', 
+    //     html: true, 
+    //     title: function() {
+    //       var d = this.__data__;
+    // var pDate = d.date;
+    //       return 'Date: ' + pDate.getDate() + " " + monthNames[pDate.getMonth()] + " " + pDate.getFullYear() + '<br>temp: ' + d.temp; 
+    //     }
+    //   });
+}
+ 
+  // create a simple data array that we'll plot with a line (this array represents only the Y temps, X will just be the index location)
   $.ajax({
     url: /\/users\/\d+/.exec(document.URL)[0],
     dataType: "JSON",
     success: function(response){
-      // duplicating the data
-      debugger
-      var data = response,
-          low = d3.min(response) - 10,
-          high = d3.max(response) + 10;
-
-      // X scale will fit all values from data[] within pixels 0-w
-      var x = d3.scale.linear().domain([0, data.length]).range([0, w]);
-      // Y scale will fit values from 0-10 within pixels h-0 (Note the inverted domain for the y-scale: bigger is up!)
-      var y = d3.scale.linear().domain([low, high]).range([h, 0]);
-        // automatically determining max range can work something like this
-        // var y = d3.scale.linear().domain([0, d3.max(data)]).range([h, 0]);
-
-      // create a line function that can convert data[] into x and y points
-      var line = d3.svg.line()
-        // assign the X function to plot our line as we wish
-        .x(function(d,i) { 
-          // verbose logging to show what's actually being done
-          console.log('Plotting X value for data point: ' + d + ' using index: ' + i + ' to be at: ' + x(i) + ' using our xScale.');
-          // return the X coordinate where we want to plot this datapoint
-          return x(i); 
-        })
-        .y(function(d) { 
-          // verbose logging to show what's actually being done
-          console.log('Plotting Y value for data point: ' + d + ' to be at: ' + y(d) + " using our yScale.");
-          // return the Y coordinate where we want to plot this datapoint
-          return y(d); 
-        });
-
-        // Add an SVG element with the desired dimensions and margin.
-        var graph = d3.select("#graph").append("svg:svg")
-              .attr("width", w + m[1] + m[3])
-              .attr("height", h + m[0] + m[2])
-            .append("svg:g")
-              .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
-
-        // create yAxis
-        var xAxis = d3.svg.axis().scale(x).tickSize(-h).tickSubdivide(true);
-        // Add the x-axis.
-        graph.append("svg:g")
-              .attr("class", "x axis")
-              .attr("transform", "translate(0," + h + ")")
-              .call(xAxis);
-
-
-        // create left yAxis
-        var yAxisLeft = d3.svg.axis().scale(y).ticks(4).orient("left");
-        // Add the y-axis to the left
-        graph.append("svg:g")
-              .attr("class", "y axis")
-              .attr("transform", "translate(-25,0)")
-              .call(yAxisLeft);
-        
-          // Add the line by appending an svg:path element with the data line we created above
-        // do this AFTER the axes above so that the line is above the tick-lines
-          graph.append("svg:path").attr("d", line(data));
-
+      draw(response);
+  
     },
     error: function(response){
       console.log("error");
