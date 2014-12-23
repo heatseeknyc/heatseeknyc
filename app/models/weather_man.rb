5f07f164e76cabcd172cfe5827c637d069a56074
Oliver Azevedo Barnes <oliverbwork@gmail.com>
2014-12-23 20:04:49 -0200

    Move services and libs out of app/models/

--- app/models/weather_man.rb
+++
@@ -1,9 +0,0 @@
-class WeatherMan
-  def self.current_outdoor_temp(zip_code)
-	Rails.cache.fetch("current_outdoor_temp", :expires_in => 15.minutes) do
-	  w_api = Wunderground.new(ENV["WUNDERGROUND_API_KEY"])
-	  json_object = w_api.conditions_for(zip_code)
-	  json_object["current_observation"]["temp_f"]
-	end    
-  end
-end
--- spec/services/pdf_writer_spec.rb
+++ spec/services/pdf_writer_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'pdf_writer'
 
 describe PDFWriter do
   describe "#generate_pdf" do
