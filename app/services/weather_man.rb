cd38bed122b76263cb77d10d37dc8529ea9a0873
williamcodes <wm.jeffries@gmail.com>
2015-02-23 21:15:44 -0500

    Increase caching of historical weather data

--- app/services/weather_man.rb
+++ app/services/weather_man.rb
@@ -2,7 +2,7 @@ class WeatherMan
   @w_api = Wunderground.new(ENV["WUNDERGROUND_API_KEY"])
 
   def self.current_outdoor_temp(zip_code)
-    key = "current_outdoor_temp_for_#{zip_code}"
+    key = "outdoor_temp_for_#{zip_code}_at_#{Time.now.strftime '%Y-%m-%dT%I'}"
     Rails.cache.fetch(key, :expires_in => 15.minutes) do
       json_object = @w_api.conditions_for(zip_code)
       json_object["current_observation"]["temp_f"]
@@ -12,7 +12,7 @@ class WeatherMan
   def self.outdoor_temp_for(time, zip_code)
     key = "outdoor_temp_for_#{zip_code}_at_#{time.strftime '%Y-%m-%dT%I'}"
     Rails.cache.fetch(key, :expires_in => 1.day) do
-      sleep 1
+      sleep 6
       observations = @w_api.history_for(time, zip_code)['history']['observations']
       observations.each do |o|
         if o['date']['hour'] == time.hour.to_s
