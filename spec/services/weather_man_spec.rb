993df34c2b052e90be73edb20b3ef3d6df6b4915
williamcodes <wm.jeffries@gmail.com>
2015-02-24 21:50:55 -0500

    add test coverage for WeatherMan service

--- spec/services/weather_man_spec.rb
+++ spec/services/weather_man_spec.rb
@@ -2,10 +2,18 @@ require 'spec_helper'
 
 describe WeatherMan do
   describe ".current_outdoor_temp" do
-    it "returns outdoor temperature from Wunderground API" do
+    it "returns current outdoor temperature from Wunderground API" do
       VCR.use_cassette('wunderground') do
         temperature = WeatherMan.current_outdoor_temp(10004)
         expect(temperature).to be_an Integer
+      end
+    end
+
+    it "returns historical outdoor temperature from Wunderground API" do
+      VCR.use_cassette('wunderground') do
+        date = Time.parse("Feb 20, 2015 at 8pm")
+        temperature = WeatherMan.outdoor_temp_for(date, 10004)
+        expect(temperature).to eq 15
       end
     end
   end
