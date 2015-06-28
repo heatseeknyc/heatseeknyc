3472e916cb47a0b5ac28849cbc85414cca6a6784
William Jeffries <wm.jeffries@gmail.com>
2015-06-28 15:36:20 -0400

    Change Paypal donate button to NYC Charities

--- app/assets/javascripts/collaboration.js
+++ app/assets/javascripts/collaboration.js
@@ -17,27 +17,3 @@ var addUserFunction = function () {
   });
 }
 
-// Donation Button on index page
-var donatePop = function() {
-  $('.donate-pop').toggleClass('hidden');
-}
-
-$(document).ready(function() {
-  $('#mc_embed_signup form input.paypal-btn').mouseover(function(){
-    $('.pp-submit-btn').css("background-color","#CCC");
-  });
-
-  $('#mc_embed_signup form input.paypal-btn').mouseout(function(){
-    $('.pp-submit-btn').css("background-color","#F2F2F2");
-  });
-
-  // open donate modal
-  $('a.donate-btn').click(function() {
-    donatePop();
-  });
-
-  // close donate modal
-  $('p.donate-dismiss').click(function() {
-    donatePop();
-  });
-});
