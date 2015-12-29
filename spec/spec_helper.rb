ed5840a7968c0ed70d308252ef059393eb8b2b98
William Jeffries <wm.jeffries@gmail.com>
2015-12-29 14:37:41 -0500

    Strip whitespace

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,13 +28,13 @@ Spork.prefork do
     config.include Devise::TestHelpers, :type => :controller
 
     config.treat_symbols_as_metadata_keys_with_true_values = true
-    
+
     config.filter_run :focus => true
-    
+
     config.run_all_when_everything_filtered = true
-    
+
     config.fixture_path = "#{::Rails.root}/spec/fixtures"
-    
+
     config.use_transactional_fixtures = true
 
     config.infer_base_class_for_anonymous_controllers = false
