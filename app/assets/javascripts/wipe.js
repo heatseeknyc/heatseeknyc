0cad8f94a0a2244cd1ddc9d2e0a175e653776993
danaavesar <danaavesar@gmail.com>
2016-01-04 20:57:22 -0800

    overlay

---
+++ app/assets/javascripts/wipe.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+$("#overlay-down-arrow").click(function(){
+	console.log("click");
+	$(this).css({
+		"margin-top" : "-100vh";
+	})
+});
+
+)};
---
+++ vendor/assets/javascripts/ScrollMagic.js
@@ -0,0 +1,44 @@
+/*!
+ * @file ScrollMagic main library.
+ */
+ /**
+ * @namespace ScrollMagic
+ */
+(function (root, factory) {
+	if (typeof define === 'function' && define.amd) {
+		// AMD. Register as an anonymous module.
+		define(factory);
+	} else if (typeof exports === 'object') {
+		// CommonJS
+		module.exports = factory();
+	} else {
+		// Browser global
+		root.ScrollMagic = factory();
+	}
+}(this, function () {
+	"use strict";
+
+	var ScrollMagic = function () {
+		_util.log(2, '(COMPATIBILITY NOTICE) -> As of ScrollMagic 2.0.0 you need to use \'new ScrollMagic.Controller()\' to create a new controller instance. Use \'new ScrollMagic.Scene()\' to instance a scene.');
+	};
+
+	ScrollMagic.version = "%VERSION%";
+
+	// TODO: temporary workaround for chrome's scroll jitter bug
+	window.addEventListener("mousewheel", function () {});
+
+	// global const
+	var PIN_SPACER_ATTRIBUTE = "data-scrollmagic-pin-spacer";
+
+// @include('ScrollMagic/Controller.js')
+
+// @include('ScrollMagic/Scene.js')
+
+// @include('ScrollMagic/Event.js')
+
+// @include('ScrollMagic/_util.js')
+
+// @generate PlugInWarnings
+
+	return ScrollMagic;
+}));
