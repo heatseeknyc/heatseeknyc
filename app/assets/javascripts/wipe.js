e8c835cfb3c97f5323afd63c68b886a2f2a67ea5
danaavesar <danaavesar@gmail.com>
2016-01-08 23:38:40 -0800

    fixed background on android

--- app/assets/javascripts/wipe.js
+++ app/assets/javascripts/wipe.js
@@ -1,31 +1,31 @@
 $(document).ready(function(){
-	// init controller
-	var controller = new ScrollMagic.Controller();
+  // init controller
+  var controller = new ScrollMagic.Controller();
 
-	// define movement of panels
-	var wipeAnimation = new TimelineMax()
-	  
-	.fromTo("#overlay", 1, {
-	    y: "0"
-	}, {
-	    y: "-100%",
-	    ease: Linear.easeNone
-	}) 
+  // define movement of panels
+  var wipeAnimation = new TimelineMax()
+    
+  .fromTo("#overlay", 1, {
+      y: "0"
+  }, {
+      y: "-100%",
+      ease: Linear.easeNone
+  }) 
 
-	// create a scene
-	new ScrollMagic.Scene({
-	        triggerElement: "#pin-container",
-	    	triggerHook: "onLeave",
-	    	duration: "110%"
-	    })
-	    .setPin("#pin-container")
-	    .setTween(wipeAnimation)
-	    .addTo(controller);
+  // create a scene
+  new ScrollMagic.Scene({
+          triggerElement: "#pin-container",
+        triggerHook: "onLeave",
+        duration: "110%"
+      })
+      .setPin("#pin-container")
+      .setTween(wipeAnimation)
+      .addTo(controller);
 
-	var heightOfOverlay = "621px";
+  var heightOfOverlay = "621px";
 
-	$(".overlay-arrow").click(function(){
-		$("body").animate({scrollTop:heightOfOverlay}, "3000");
-	})
+  $(".overlay-arrow").click(function(){
+    $("body").animate({scrollTop:heightOfOverlay}, "3000");
+  })
 
 });
