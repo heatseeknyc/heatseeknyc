b3979beb8863fc293f61eca412821cf6f978e2da
danaavesar <danaavesar@gmail.com>
2016-01-10 12:12:04 -0800

    fixed chrome android issue

--- app/assets/javascripts/wipe.js
+++ app/assets/javascripts/wipe.js
@@ -1,3 +1,5 @@
+
+
   $(document).ready(function(){
     $(".overlay-arrow").click(function(){
      $("body,html").animate({scrollTop:"240px"}, "3000");
@@ -12,6 +14,21 @@
           "-0-transform" : "translate(0%,"+ -scroll*.3+"%) matrix(1, 0, 0, 1, 0, 0)"
         })
      });
+
+//----for Chrome on Android ----//
+    var bg = $("#overlay");
+    $(window).resize("resizeBackground");
+    function resizeBackground() {
+        bg.height(jQuery(window).height());
+    }
+    resizeBackground();
+    if (navigator.appVersion.indexOf("Chrome/") != -1) {
+      if(window.screen.height <= 640 && window.screen.width <= 360){
+        $("#overlay").css({
+          "background": "url('assets/city-background-mobile.jpg') 0% / 100% 500px no-repeat"
+        })
+      }
+    }
+
     
   });
-  
