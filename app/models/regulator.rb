fdb4eb3dadfb8d0051dbc3745beec6ef12539bb9
William Jeffries <wm.jeffries@gmail.com>
2016-01-11 14:27:31 -0500

    Create regulator

--- app/models/regulator.rb
+++ app/models/regulator.rb
@@ -1,12 +1,18 @@
 # TODO: replace Regulatable module with Regulator class
 class Regulator
-  attr_reader :reading
-  def initialize(reading)
-    @reading = reading
+  attr_reader :readings
+  def initialize(readings)
+    if readings.respond_to?(:each)
+      @readings = readings
+    else
+      @readings = [readings]
+    end
   end
 
   def has_detected_violation?
-    r = reading
-    User.new.in_violation?(r.created_at, r.outdoor_temp, r.temp)
+    readings.any? do |r|
+      # TODO: make it unnecessary to instantiate another class for this
+      User.new.in_violation?(r.created_at, r.outdoor_temp, r.temp)
+    end
   end
 end
--- spec/models/regulator_spec.rb
+++ spec/models/regulator_spec.rb
@@ -2,11 +2,22 @@ require 'spec_helper'
 
 describe Regulator do
   describe "#self.in_violation?" do
-    it "returns violation status of reading", :focus do
+    it "returns violation status of a single reading" do
       reading = create(:reading, temp: 50, outdoor_temp: 38)
       regulator = Regulator.new(reading)
       expect(regulator).to have_detected_violation
       reading.outdoor_temp = 70
+      expect(regulator).to_not have_detected_violation
+    end
+
+    it "returns violation status of multiple readings" do
+      reading1 = create(:reading, temp: 50, outdoor_temp: 38)
+      reading2 = create(:reading, temp: 70, outdoor_temp: 38)
+      readings = [reading1, reading2]
+
+      regulator = Regulator.new(readings)
+      expect(regulator).to have_detected_violation
+      reading1.outdoor_temp = 70
       expect(regulator).to_not have_detected_violation
     end
   end
