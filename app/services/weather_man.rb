ea63ba2e8d7c0f66a9e5f7d40d2025fc22dc63db
William Jeffries <wm.jeffries@gmail.com>
2016-01-18 16:30:40 -0500

    Add .key_for method to WeatherMan class

--- app/services/weather_man.rb
+++ app/services/weather_man.rb
@@ -1,29 +1,34 @@
 class WeatherMan
   @w_api = Wunderground.new(ENV["WUNDERGROUND_API_KEY"])
 
+  def self.key_for(zip_code, datetime)
+    format = '%Y-%m-%d'
+    format += 'H%H' if datetime.today?
+
+    "outdoor_temp_for_#{zip_code}_on_#{datetime.strftime(format)}"
+  end
+
   def self.current_outdoor_temp(zip_code, throttle = 9)
     return nil if !zip_code
-    key = "outdoor_temp_for_#{zip_code}_at_#{Time.now.strftime '%Y-%m-%dT%H'}"
-    Rails.cache.fetch(key, :expires_in => 1.hour) do
+    Rails.cache.fetch(key_for(zip_code, DateTime.now), :expires_in => 1.hour) do
       sleep throttle
       json_object = @w_api.conditions_for(zip_code)
       json_object["current_observation"]["temp_f"]
     end
   end
 
-  def self.outdoor_temp_for(time, zip_code, throttle = nil)
+  def self.outdoor_temp_for(datetime, zip_code, throttle = nil)
     raise ArgumentError if !zip_code
 
     throttle ||= 9
-    key = "outdoor_temp_for_#{zip_code}_at_#{time.strftime '%Y-%m-%dT%H'}"
-    Rails.cache.fetch(key, :expires_in => 1.day) do
+    Rails.cache.fetch(key_for(zip_code, datetime), :expires_in => 1.hour) do
       sleep throttle
-      observationHash = @w_api.history_for(time, zip_code)
+      observationHash = @w_api.history_for(datetime, zip_code)
 
       if observationHash
         observations = observationHash['history']['observations']
         observations.each do |o|
-          if o['date']['hour'].to_i == time.hour.to_i
+          if o['date']['hour'].to_i == datetime.hour.to_i
             return o['tempi'].to_i
           end
         end
--- spec/services/weather_man_spec.rb
+++ spec/services/weather_man_spec.rb
@@ -1,6 +1,18 @@
 require 'spec_helper'
 
 describe WeatherMan do
+  describe ".key_for" do
+    it "returns a key for a given zip code and datetime" do
+      key = WeatherMan.key_for(10001, DateTime.parse('January 1, 2016'))
+      expect(key).to eq "outdoor_temp_for_10001_on_2016-01-01"
+    end
+
+    it "includes the hour if datetime is today" do
+      key = WeatherMan.key_for(10001, DateTime.now)
+      expect(key).to include "H#{DateTime.now.hour}"
+    end
+  end
+
   describe ".current_outdoor_temp" do
     it "returns current outdoor temperature from Wunderground API" do
       VCR.use_cassette('wunderground') do
