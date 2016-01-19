ad255cab77a1af73c2f9ce9a5ac5c4c3cab78246
William Jeffries <wm.jeffries@gmail.com>
2016-01-19 11:40:22 -0500

    Use more reliable Time.zone and clean up after time travel

--- spec/models/regulator_spec.rb
+++ spec/models/regulator_spec.rb
@@ -2,10 +2,16 @@ require 'spec_helper'
 
 describe Regulator do
   describe "#self.in_violation?" do
+    after(:all) do
+      Timecop.travel(Time.zone.parse('March 1, 2015 00:00:00 -0500'))
+    end
+
     context "during the day" do
+
       before(:each) do
-        Timecop.travel(DateTime.now.change(hour: 6))
+        Timecop.travel(Time.zone.now.change(hour: 6, offset: '-0500'))
       end
+
       it "returns violation status of a single reading" do
         reading = create(:reading, temp: 67, outdoor_temp: 54)
         regulator = Regulator.new(reading)
@@ -32,9 +38,11 @@ describe Regulator do
     end
 
     context "during the night" do
+
       before(:each) do
-        Timecop.travel(DateTime.now.change(hour: 22))
+        Timecop.travel(Time.zone.now.change(hour: 22, offset: '-0500'))
       end
+
       it "returns violation status of a single reading" do
         reading = create(:reading, temp: 54, outdoor_temp: 39)
         regulator = Regulator.new(reading)
@@ -44,7 +52,7 @@ describe Regulator do
         reading.outdoor_temp = 39
         reading.temp = 55
         expect(regulator).to_not have_detected_violation
-        reading.outdoor_temp = 55
+        reading.outdoor_temp = 40
         expect(regulator).to_not have_detected_violation
       end
 
--- spec/services/weather_man_spec.rb
+++ spec/services/weather_man_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 describe WeatherMan do
   describe ".key_for" do
     it "returns a key for a given zip code and datetime" do
-      key = WeatherMan.key_for(10001, DateTime.parse('January 1, 2015'))
+      key = WeatherMan.key_for(10001, Time.zone.parse('January 1, 2015'))
       expect(key).to eq "outdoor_temp_for_10001_on_2015-01-01"
     end
 
     it "includes the hour if datetime is today" do
-      datetime = DateTime.now
+      datetime = Time.zone.now
       key = WeatherMan.key_for(10001, datetime)
       expect(key).to eq "outdoor_temp_for_10001_on_2015-03-01H00"
     end
@@ -29,7 +29,7 @@ describe WeatherMan do
 
     it "returns historical outdoor temperature from Wunderground API" do
       VCR.use_cassette('wunderground') do
-        date = Time.parse("Feb 20, 2015 at 8pm")
+        date = Time.zone.parse("Feb 20, 2015 at 8pm")
         temperature = WeatherMan.outdoor_temp_for(date, 10004, 0)
         expect(temperature).to eq 15
       end
