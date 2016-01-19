3edb3a94a8c09aadbd58f863a883944ceb115f89
William Jeffries <wm.jeffries@gmail.com>
2016-01-19 10:17:25 -0500

    Increase test coverage around regulator

--- spec/models/regulator_spec.rb
+++ spec/models/regulator_spec.rb
@@ -2,23 +2,63 @@ require 'spec_helper'
 
 describe Regulator do
   describe "#self.in_violation?" do
-    it "returns violation status of a single reading" do
-      reading = create(:reading, temp: 50, outdoor_temp: 38)
-      regulator = Regulator.new(reading)
-      expect(regulator).to have_detected_violation
-      reading.outdoor_temp = 70
-      expect(regulator).to_not have_detected_violation
+    context "during the day" do
+      before(:each) do
+        Timecop.travel(DateTime.now.change(hour: 6))
+      end
+      it "returns violation status of a single reading" do
+        reading = create(:reading, temp: 67, outdoor_temp: 54)
+        regulator = Regulator.new(reading)
+        expect(regulator).to have_detected_violation
+        reading.outdoor_temp = 55
+        expect(regulator).to_not have_detected_violation
+        reading.outdoor_temp = 54
+        reading.temp = 68
+        expect(regulator).to_not have_detected_violation
+        reading.outdoor_temp = 55
+        expect(regulator).to_not have_detected_violation
+      end
+
+      it "returns violation status of multiple readings" do
+        reading1 = create(:reading, temp: 67, outdoor_temp: 54)
+        reading2 = create(:reading, temp: 68, outdoor_temp: 54)
+        readings = [reading1, reading2]
+
+        regulator = Regulator.new(readings)
+        expect(regulator).to have_detected_violation
+        reading1.outdoor_temp = 70
+        expect(regulator).to_not have_detected_violation
+      end
     end
 
-    it "returns violation status of multiple readings" do
-      reading1 = create(:reading, temp: 50, outdoor_temp: 38)
-      reading2 = create(:reading, temp: 70, outdoor_temp: 38)
-      readings = [reading1, reading2]
+    context "during the night" do
+      before(:each) do
+        Timecop.travel(DateTime.now.change(hour: 22))
+      end
+      it "returns violation status of a single reading" do
+        reading = create(:reading, temp: 54, outdoor_temp: 39)
+        regulator = Regulator.new(reading)
+        expect(regulator).to have_detected_violation
+        reading.outdoor_temp = 40
+        expect(regulator).to_not have_detected_violation
+        reading.outdoor_temp = 39
+        reading.temp = 55
+        expect(regulator).to_not have_detected_violation
+        reading.outdoor_temp = 55
+        expect(regulator).to_not have_detected_violation
+      end
 
-      regulator = Regulator.new(readings)
-      expect(regulator).to have_detected_violation
-      reading1.outdoor_temp = 70
-      expect(regulator).to_not have_detected_violation
+      it "returns violation status of multiple readings" do
+        reading1 = create(:reading, temp: 54, outdoor_temp: 39)
+        reading2 = create(:reading, temp: 55, outdoor_temp: 39)
+        readings = [reading1, reading2]
+
+        regulator = Regulator.new(readings)
+        expect(regulator).to have_detected_violation
+        reading1.outdoor_temp = 65
+        expect(regulator).to_not have_detected_violation
+      end
     end
+
   end
 end
