a6972ff19f62e2cc3bacfb872b115432c44a7d9c
William Jeffries <wm.jeffries@gmail.com>
2016-01-20 22:35:08 -0500

    Throw errors from VCR when there are unused HTTP interactions

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -3,7 +3,11 @@ require 'vcr'
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/fixtures/vcr'
   c.hook_into :webmock
-  c.default_cassette_options = { serialize_with: :json, record: :once }
+  c.default_cassette_options = {
+    serialize_with: :json,
+    record: :once,
+    allow_unused_http_interactions: false
+  }
   c.debug_logger = File.open(Rails.root.join('log', 'vcr.log'), 'a')
   c.filter_sensitive_data('---WUNDERGROUND_KEY---') { ENV['WUNDERGROUND_KEY'] }
   c.configure_rspec_metadata!
