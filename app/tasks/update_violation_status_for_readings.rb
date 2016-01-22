bb521b3e691dbbab0d9ef70bca314ec1ecdb84e2
William Jeffries <wm.jeffries@gmail.com>
2016-01-21 22:38:12 -0500

    Silence output from tasks in test suite

--- app/tasks/update_violation_status_for_readings.rb
+++ app/tasks/update_violation_status_for_readings.rb
@@ -1,6 +1,6 @@
 class UpdateViolationStatusForReadings
-  def self.exec
+  def self.exec(options)
     readings = Reading.where.not(outdoor_temp: nil)
-    Regulator.new(readings).batch_inspect!
+    Regulator.new(readings).batch_inspect!(options)
   end
 end
