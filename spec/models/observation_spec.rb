03042b06fa77f2a5e3a08fd99f706d955c595d2f
William Jeffries <wm.jeffries@gmail.com>
2016-01-28 19:27:42 -0500

    Upgrade to v2.0 of WUnderground API

--- spec/models/observation_spec.rb
+++ spec/models/observation_spec.rb
@@ -1,16 +1,14 @@
 require 'spec_helper'
 
 describe Observation do
-  describe ".new_from_hash" do
-    it "returns an Observation" do
-      response = {
-        'date' => {
-          'hour' => '07'
-        },
-        'tempi' => '45.8'
-      }
-      o = Observation.new_from_hash(response)
-      expect(o.temperature).to eq 45.8
+  describe ".new_from_raw" do
+    let(:time) { Time.zone.parse("March 1, 2015 at 12pm") }
+    let(:response) { WeatherMan.api.history_for(time, "knyc") }
+    let(:raw_observation) { response["history"]["days"][0]["observations"][0] }
+
+    it "returns an Observation", :vcr do
+      observation = Observation.new_from_raw(raw_observation)
+      expect(observation.temperature).to eq 26.1
     end
   end
 end
