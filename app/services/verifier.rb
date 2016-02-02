b84c36e1d734f3e378d52d9c4d622ce06ea6fb04
William Jeffries <wm.jeffries@gmail.com>
2016-02-02 14:58:34 -0500

    Return 200 for dupes but don't save them

--- app/services/verifier.rb
+++ app/services/verifier.rb
@@ -1,6 +1,29 @@
 class Verifier
   attr_accessor :sensor_name, :temp, :time, :verification
 
+  RESPONSE_MAP = {
+    ok: {
+      error_message: "",
+      status: 200
+    },
+    duplicate_reading: {
+      error_message: "A reading already exists for that user at that time",
+      status: 200
+    },
+    bad_verification: {
+      error_message: "Bad verification string",
+      status: 400
+    },
+    missing_sensor: {
+      error_message: "No sensor by that name found",
+      status: 400
+    },
+    missing_user: {
+      error_message: "No user associated with that sensor",
+      status: 400
+    }
+  }.freeze
+
   def initialize(params)
     @sensor_name = params[:sensor_name]
     @temp = params[:temp]
@@ -8,8 +31,16 @@ class Verifier
     @verification = params[:verification]
   end
 
+  def result
+    return :bad_verification if !good_verification?
+    return :missing_sensor if !sensor?
+    return :missing_user if !user?
+    return :duplicate_reading if dupe?
+    :ok
+  end
+
   def passing?
-    good_verification? && sensor? && user? && !dupe?
+    good_verification? && sensor? && user?
   end
 
   def failing?
@@ -26,7 +57,10 @@ class Verifier
 
   def dupe
     created_at = Time.at(time.to_i)
-    Reading.find_by(sensor: sensor, created_at: created_at) || NullObject.new
+    Reading.find_by(
+      sensor_id: sensor.id,
+      created_at: created_at
+    ) || NullObject.new
   end
 
   def good_verification?
@@ -34,21 +68,34 @@ class Verifier
   end
 
   def sensor?
-    !sensor.nil?
+    !missing_sensor?
+  end
+
+  def missing_sensor?
+    sensor.nil?
   end
 
   def user?
-    !sensor.user.nil?
+    !missing_user?
+  end
+
+  def missing_user?
+    sensor.user.nil?
   end
 
   def dupe?
-    !dupe.nil?
+    !unique?
+  end
+
+  def unique?
+    dupe.nil?
+  end
+
+  def status
+    RESPONSE_MAP[result][:status]
   end
 
   def error_message
-    return "Bad verification string" if !good_verification?
-    return "No sensor by that name found" if !sensor?
-    return "No user associated with that sensor" if !user?
-    return "Already a reading for that sensor at that time" if dupe?
+    RESPONSE_MAP[result][:error_message]
   end
 end
