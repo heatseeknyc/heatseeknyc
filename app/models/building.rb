c819b5f7e2ff62ab52ccba305fe0483d100e969c
Ramil Lim <ramil.lim@gmail.com>
2016-05-25 12:46:43 -0400

    Create basic Building model and associations to User model

---
+++ app/models/building.rb
@@ -0,0 +1,8 @@
+class Building < ActiveRecord::Base
+  has_many :tenants, class_name: User.name
+
+  validates_presence_of :street_address, :zip_code
+  validates_format_of :zip_code,
+                      with: /\A\d{5}-\d{4}|\A\d{5}\z/,
+                      message: "should be 12345 or 12345-1234"
+end
---
+++ spec/factories/buildings.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :building do
+    street_address "11 Broadway"
+    zip_code "10004"
+  end
+end
