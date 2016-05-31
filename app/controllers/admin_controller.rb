d2af6fca0a5d6489ea57f8c6238bb495e83d9a65
Ramil Lim <ramil.lim@gmail.com>
2016-05-31 12:07:29 -0400

    Add user_signed_in? check to AdminController

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,6 +4,6 @@ class AdminController < ApplicationController
   private
 
   def validate_admin_user!
-    redirect_to root_path unless current_user.admin?
+    redirect_to root_path unless user_signed_in? && current_user.admin?
   end
 end
