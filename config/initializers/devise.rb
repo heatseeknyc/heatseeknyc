37290edd3cefaf35af3bb919e5fb2410f3565d54
William Jeffries <wm.jeffries@gmail.com>
2016-07-01 10:58:12 -0400

    Update actionmailer for new domain name

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -15,7 +15,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'no-reply@heatseeknyc.com'
+  config.mailer_sender = 'no-reply@heatseek.org'
 
   # Configure the class responsible to send e-mails.
   config.mailer = 'Devise::Mailer'
