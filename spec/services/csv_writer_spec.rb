87ff8d27bd12e97b93ac7f907c09217d0669a5aa
Ryan Boland <bolandryanm@gmail.com>
2017-11-01 11:22:55 -0400

    update tests

--- spec/services/csv_writer_spec.rb
+++ spec/services/csv_writer_spec.rb
@@ -8,13 +8,13 @@ describe CSVWriter do
   describe "#generate_csv" do
     it "generates a csv for a user with readings" do
       reading1 = create(:reading, user: user)
-      reading2 = create(:reading, :violation, user: user)
+      reading2 = create(:reading, :violation, user: user, humidity: 43.2452)
       generated_csv = writer.generate_csv
       time = "%l:%M%p"
       date = "%m/%d/%Y"
-      expect(generated_csv).to include("TIME,DATE,TEMP INSIDE,TEMP OUTSIDE,TEMP OF HOT WATER,NOTES")
-      expect(generated_csv).to include("#{reading1.created_at.strftime(time)},#{reading1.created_at.strftime(date)},#{reading1.temp},#{reading1.outdoor_temp},,")
-      expect(generated_csv).to include("#{reading2.created_at.strftime(time)},#{reading2.created_at.strftime(date)},#{reading2.temp},#{reading2.outdoor_temp},,violation")
+      expect(generated_csv).to include("TIME,DATE,TEMP INSIDE,HUMIDITY INSIDE (%),TEMP OUTSIDE,TEMP OF HOT WATER,NOTES")
+      expect(generated_csv).to include("#{reading1.created_at.strftime(time)},#{reading1.created_at.strftime(date)},#{reading1.temp},,#{reading1.outdoor_temp},,")
+      expect(generated_csv).to include("#{reading2.created_at.strftime(time)},#{reading2.created_at.strftime(date)},#{reading2.temp},43.2,#{reading2.outdoor_temp},,violation")
     end
 
     it "returns a filename" do
@@ -23,7 +23,7 @@ describe CSVWriter do
 
     it "generates a csv for a user without readings" do
       generated_csv = writer.generate_csv
-      expect(generated_csv).to include("TIME,DATE,TEMP INSIDE,TEMP OUTSIDE,TEMP OF HOT WATER,NOTES")
+      expect(generated_csv).to include("TIME,DATE,TEMP INSIDE,HUMIDITY INSIDE (%),TEMP OUTSIDE,TEMP OF HOT WATER,NOTES")
     end
   end
 end
