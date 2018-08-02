6bebafae3aedcae8f8c490656bed88948ec5c45b
Ryan Boland <bolandryanm@gmail.com>
2018-08-02 11:24:08 -0400

    disable asset compilation on prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Twinenyc::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
