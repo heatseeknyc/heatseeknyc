0f2c1775585edd1d3dc3dd53f13dd719c04ce6c9
Eric Skiff <eric@tanookilabs.com>
2019-03-15 11:26:07 -0400

    Initial import of back readings to CanonicalTemperature

--- app/models/canonical_temperature.rb
+++ app/models/canonical_temperature.rb
@@ -16,4 +16,28 @@ class CanonicalTemperature < ActiveRecord::Base
     end
     canonical_temp
   end
+
+  def self.import_meteoblue_readings(filename)
+    require 'csv'    
+    zips = User.pluck(:zip_code).uniq
+    # NOTE: we drop the first 12 lines to conform to the meteoblue raw output! 
+    # Don't cut them from the file
+    CSV.parse(File.readlines(filename).drop(12).join, { headers: false, col_sep: ";" } ) do |r|
+      record_time = DateTime.parse("" + r.slice(0..2).join("-") + " " + r[3] + ":" + r[4] + " -0400")
+      temp = r[5]
+      zips.each do |zip|
+        canonical_temp = CanonicalTemperature.find_by({
+          zip_code: zip,
+          record_time: record_time
+        })
+        if canonical_temp.nil?
+          CanonicalTemperature.create({
+            zip_code: zip,
+            record_time: record_time,
+            outdoor_temp: temp
+          })
+        end
+      end
+    end
+  end
 end
